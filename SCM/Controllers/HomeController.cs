ccb23a536aa419f04c31a47142072c64cf6c8b8a
宁向 <1772105645@qq.com>
2021-04-30 10:01:22 +0800

    框架

---
+++ SCM/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SCM.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Login()
+        {
+            return View();
+        }
+        public ActionResult main()
+        {
+            return View();
+        }
+    }
+}
