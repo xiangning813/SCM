f88e6521ab6d47e2c0b7627cfb37f2ff52d58ad4
al11-ql <3469063312@qq.com>
2021-05-04 12:02:18 +0800

    aa

--- SCM/Controllers/BuyController.cs
+++ SCM/Controllers/BuyController.cs
@@ -3,17 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Net.Http;
 
 namespace SCM.Controllers
 {
     public class BuyController : Controller
     {
+        
         // GET: Buy
         public ActionResult Index()
         {
             return View();
         }
-        public ActionResult Depot()
+        public ActionResult Depot(int SIDID,string PPName,string UsersName,int SIDData)
         {
             return View();
         }
